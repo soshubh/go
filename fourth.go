5fbc54e2e0362744be46cc9c1c95cd64960e83df
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-12-01 14:06:04 +0530

    Create fourth.go

---
+++ fourth.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main(){
+    var(
+        a int
+        b bool=false
+        c string="shubh")
+        
+    fmt.Println(a, b, c)
+    
+    var temp int
+    fmt.Print(temp)
+}
