68a1c5dffcb7f59142a5479f95ace008c7ee06a3
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-12-01 13:48:27 +0530

    Create second.go

---
+++ second.go
@@ -0,0 +1,17 @@
+// Online Go compiler to run Golang program online
+// Print "Try programiz.pro" message
+
+package main
+import "fmt"
+
+func main() {
+  var first string 
+  var second int
+  var third bool
+  fmt.Print(first, second, third, "\n")
+  
+  fmt.Println(first+"This is string value if there is nothing thats mean its empty")
+  fmt.Println(second)
+  fmt.Println(third)
+  
+}
