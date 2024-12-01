f64be167e00b5e09298c23282ecf9e194bc65a17
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-12-01 13:47:56 +0530

    Create first.go

---
+++ first.go
@@ -0,0 +1,21 @@
+package main
+import ("fmt")
+
+func main(){
+    fmt.Print("Hello\n")
+    
+    var first string = "Shubh"
+    fmt.Print(first+"\t")
+    
+    var first_1 = "singh"
+    fmt.Print(first_1+"\n")
+    
+    var second int = 755
+    fmt.Print(second,"\n")
+    
+    isfloat := 5.43
+    fmt.Print(isfloat,"\n")
+    
+    isbool := true
+    fmt.Print(isbool,"\n")
+}
